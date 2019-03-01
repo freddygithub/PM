bf5e4f8142674ae26e0a99acac3db8cd4235b195
Freddy <fedmaster217@yahoo.com>
2019-03-01 16:05:06 -0500

    saviing all data before working users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PMTodo';
+   todoArray=[]
+   todoDesArray=[]
+   
+   addTodo(value, des){
+    this.todoArray.push(value)
+    this.todoDesArray.push(des)
+    console.log(this.todoArray)
+  }
+  
+  deleteItem(todo){
+   for(let i=0 ;i<= this.todoArray.length ;i++){
+    if(todo== this.todoArray[i]){
+     this.todoArray.splice(i,1)
+    }
+   }
+  }
 }
