676d300db2e17bc10593f651de83481c5bad0df5
Freddy <fedmaster217@yahoo.com>
2019-03-08 13:59:50 -0500

    changing buttons on task view

---
+++ PMTodo/assets/js/main.js
@@ -0,0 +1,116 @@
+function sortList() {
+  var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("id02");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+      
+      
+    // start by saying: no switching is done:
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    // Loop through all list-items:
+    for (i = 0; i < (b.length - 1); i++) {
+      // start by saying there should be no switching:
+      shouldSwitch = false;
+      /* check if the next item should
+      switch place with the current item: */
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        /* if next item is alphabetically
+        lower than current item, mark as a switch
+        and break the loop: */
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark the switch as done: */
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav2() {
+ document.getElementById("mySidenav2").style.width = "400px";
+ document.getElementById("main2").style.marginLeft = "400px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav2() {
+  document.getElementById("mySidenav2").style.width = "0";
+  document.getElementById("main2").style.marginLeft = "0";
+} 
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			noOpenerFade: true,
+			hoverDelay: 150,
+			hideDelay: 350
+		});
+
+	// Nav.
+
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+})(jQuery);
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
  
+   selectedTitle = "";
+   selectedDes = "";
+   selectedCat = "";
+   selectedDate = "";
+   
+   deviceObjects = [{name: 'Issue'}, {name: 'New Feature'}, {name: 'Feature Fix'}];
+   selectedDeviceObj = this.deviceObjects[1];
+   
    todoArray=[{name: "Project management website", des: "the text on the description of the tasks are to small and make the main page look weird without a lot  of text.", date: "2019-01-07"},
    {name: "Mirror", des: "the text on the description of the tasks are to small and make the main page look weird without a lot of text.", date: "2019-10-07"},
    {name: "Bike", des: "the text on the description of the tasks are to small and make the main page look weird without a lot of text.", date: "2019-12-07"}]
@@ -15,5 +23,32 @@ export class ProjectComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  
+  onChangeObj(newObj) {
+    this.selectedDeviceObj = newObj;
+  }
+   
+   selectedButton(value){
+   this.selectedTitle = value.name
+   this.selectedDes = value.des
+   this.selectedCat = value.cat
+   this.selectedDate = value.date
+   }
+   
+   addTodo(value, des, date){
+        this.todoArray.push({
+        name: value,
+        des: des,
+        date: date
+        })
+  }
+  
+  deleteItem(todo){
+   for(let i=0 ;i<= this.todoArray.length ;i++){
+    if(todo== this.todoArray[i]){
+     this.todoArray.splice(i,1)
+    }
+   }
+  }
+   
 }
